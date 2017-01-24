2aa4c274c3241987f76b4a9fe19c418d47f5be7f
gaisei makimura <futuredictator30@yahoo.co.jp>
2017-01-25 01:39:21 +0900

    新着アニメ情報、新着キャラ情報をjsonからajaxで出力

--- js/app.js
+++ js/app.js
@@ -24,9 +24,114 @@ var Header = React.createClass({
 });
 
 ReactDOM.render(
-	<Header title="萌え萌えランキング" concept="いい年した親父が萌えキャラをひとりじめするバカみたいなサイトです...死ねばいいのに！" />,
+	<Header title="萌え萌えランキング" concept="コミュ障の親父が萌えキャラをひとりじめするバカみたいなサイトです...死ねばいいのに！" />,
 	document.getElementById('header')
 );
+
+
+/* 新着アニメ情報
+****************************************************/
+var NewAnimeInfo = React.createClass({
+	displayName: 'NewAnimeInfo',
+	getInitialState: function() {
+		return {data: []};
+	},
+	componentDidMount: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	render: function() {
+		var stateData = this.state.data
+		var newAnime3 = stateData.reverse().slice(0, 3);
+		return (
+			<ul>
+				{newAnime3.map(function(result, i) {
+				var newAnimeThumbnail = "images/anime/" + result.id + "/thumbnail.jpg";
+					return (
+						<li key={'key_' + i}>
+							<a href="#">
+								<figure>
+									<img src={newAnimeThumbnail} alt={result.name} width="250" height="250" />
+									<figcaption>{result.name}</figcaption>
+								</figure>
+							</a>
+						</li>
+					);
+				})}
+			</ul>
+		);
+	}
+});
+
+ReactDOM.render(
+	<NewAnimeInfo url="js/data/anime.json" />,
+	document.querySelector('.newAnimeList')
+);
+
+
+
+/* 新着キャラクター情報
+****************************************************/
+var NewCharaInfo = React.createClass({
+	displayName: 'NewCharaInfo',
+	getInitialState: function() {
+		return {data: []};
+	},
+	componentDidMount: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	render: function() {
+		var stateData = this.state.data
+		var newChara3 = stateData.reverse().slice(0, 5);
+		return (
+			<ul>
+				{newChara3.map(function(result, i) {
+					if(result.animeid !== null) {
+						var newCharaThumbnail = "images/anime/" + result.animeid + "/character/" + result.charaid + ".jpg";
+					} else {
+						/* nullにしてるのはアニメ用画像を作るのが面倒だったから。あとでちゃんと作る。 */
+						var newCharaThumbnail = "images/anime/null/character/" + result.charaid + ".jpg";
+					}
+					return (
+						<li key={'key_' + i}>
+							<a href="#">
+								<figure>
+									<img src={newCharaThumbnail} alt={result.name} width="150" height="200" />
+									<figcaption>{result.name}</figcaption>
+								</figure>
+							</a>
+						</li>
+					);
+				})}
+			</ul>
+		);
+	}
+});
+
+ReactDOM.render(
+	<NewCharaInfo url="js/data/character.json" />,
+	document.querySelector('.newCharaList')
+);
+
 
 
 /* サイドバー
