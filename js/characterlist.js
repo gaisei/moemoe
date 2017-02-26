ec2d2969e700d74365968db207f30570b8f15f9d
gaisei makimura <futuredictator30@yahoo.co.jp>
2017-02-26 16:02:54 +0900

    キャラクター一覧ページにページング

--- js/characterlist.js
+++ js/characterlist.js
@@ -5,7 +5,10 @@ var AnimeData = [];
 var CharaList = React.createClass({
 	displayName: 'CharaList',
 	getInitialState: function() {
-		return {data: []};
+		return {
+			data: [],
+			page: 1
+		};
 	},
 	componentDidMount: function() {
 		$.ajax({
@@ -25,22 +28,49 @@ var CharaList = React.createClass({
 			AnimeData = data;
 		});
 	},
+	pageLink: function(num) {
+		var pageLinkNum = Math.ceil(+num / 20);
+		var pageLinks = [];
+		for(var i = 1; i <= pageLinkNum; i++) {
+			if(this.state.page == i) {
+				pageLinks.push(<li key={i} value={i} className="active" onClick={this.moveLink}>{i}</li>);
+			} else {
+				pageLinks.push(<li key={i} value={i} onClick={this.moveLink}>{i}</li>);
+			}
+		}
+		return pageLinks;
+	},
+	moveLink: function(e) {
+		var pageNum = e.target.value;
+		location.hash = '#page' + pageNum;
+	},
 	render: function() {
 		var stateData = this.state.data;
-		var charaList = stateData.reverse()/*.slice(0, 20)*/; /* 初期表示 登録順20件 */
+		var charaTotalNum = Object.keys(stateData).length;
+
+		var hash = location.hash;
+		var page = hash.replace(/#page/g, '').split('/')[0];
+		if(page == '') page = 1;
+		this.state.page = page;
+		
+		var pageStart = page == 1 ? 0 : (page-1) * 20;
+		var pageEnd = page * 20;
+		var charaList = stateData.reverse().slice(pageStart, pageEnd); /* 初期表示 登録順20件 */
+
 		this.getAnimeData();
 		return (
-			<ul>
-				{charaList.map(function(result, i) {
-					var animeid = result.animeid;
-					var animeName;
-					for(var j = 0; j < AnimeData.length; j++) {
-						if(AnimeData[j].id == animeid) {
-							animeName = AnimeData[j].nickname;
+			<div>
+				<ul>
+					{charaList.map(function(result, i) {
+						var animeid = result.animeid;
+						var animeName;
+						for(var j = 0; j < AnimeData.length; j++) {
+							if(AnimeData[j].id == animeid) {
+								animeName = AnimeData[j].nickname;
+							}
 						}
-					}
-					var charaThumbnail = "../images/anime/" + result.animeid + "/character/" + result.charaid +  ".jpg";
-					var charaUrl = "../character/#" + animeName + "/" + result.charaid;
+						var charaThumbnail = "../images/anime/" + result.animeid + "/character/" + result.charaid +  ".jpg";
+						var charaUrl = "../character/#" + animeName + "/" + result.charaid;
 						return (
 							<li key={'key_' + i}>
 								<a href={charaUrl}>
@@ -52,7 +82,9 @@ var CharaList = React.createClass({
 							</li>
 						);
 					})}
-			</ul>
+				</ul>
+				<ul className="pageLink">{this.pageLink(charaTotalNum)}</ul>
+			</div>
 		);
 	}
 });
@@ -61,7 +93,3 @@ ReactDOM.render(
 	<CharaList url="../js/data/character.json" />,
 	document.querySelector('.charaListArea')
 );
-
-
-
-
